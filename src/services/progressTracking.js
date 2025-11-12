b4a317f231b10a2c8745b22b6c8c19a177f73c24
edlight123 <info@edlight.org>
2025-11-12 04:41:39 +0000

    fix: Correct firebase import paths in new services
    - Change from '../firebase' to './firebase' in progressTracking.js
    - Change from '../firebase' to './firebase' in notificationService.js
    - Fixes build errors

--- src/services/progressTracking.js
+++ src/services/progressTracking.js
@@ -1,4 +1,4 @@
-import { db } from '../firebase';
+import { db } from './firebase';
 import { doc, getDoc, setDoc, updateDoc, arrayUnion, increment, serverTimestamp } from 'firebase/firestore';
 import { notifyAchievement, notifyStreak } from './notificationService';
 
